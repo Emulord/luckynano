2399e074fc4859d1478ec1dfbd51526d8e59936f
Steven LEFEBVRE <emulord@MacBook-Pro.local>
2020-11-14 15:13:48 +0100

    2.3 lang

--- luckynano.com/lang/es/index_ihm_lang.php
+++ luckynano.com/lang/es/index_ihm_lang.php
@@ -2,7 +2,7 @@
 $index_ihm_lang = array(
   'ihm_login_button' => '<big>Bienvenid@!</big><br/>Por favor <gold style="font-weight:600">ingresa</gold> a tu cuenta para jugar.</span>',
   'ihm_signup_button' => 'Registrarse',
-  'logged' => '<div> Bienvenidos a <logo>LUCKY</logo>NANO 2.<span class="rose">2</span> ! :-) </div>
+  'logged' => '<div> Bienvenidos a <logo>LUCKY</logo>NANO 2.<span class="rose">3</span> ! :-) </div>
     <!--<div class="button red maintenance_message" style="width:35rem; font-weight:600; padding: 0.67rem; text-align:center; font-size:80%"><img class="header_rank" src="images/level/chevron-98.svg" title="Level 98" alt="Level 98"/> v2.1 Update Maintenance planned : 2020.03.22 @ 1pm CET (1h approximately)</div>-->',
   'ihm_faucet_button_text' => 'Faucet',
 );
